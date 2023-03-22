8d4319f56b1f2bae7041e163f9af022783e416bc
snowgoos <xrustjasij@gmail.com>
2023-03-22 23:13:31 +0200

    Homework5

---
+++ Homework5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ijunior
+{
+    internal class Homework5
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "lastName";
+            string lastName = "firstName";
+            string temporaryFirstName;
+
+            Console.WriteLine($"firstname: {firstName}, lastname: {lastName}");
+
+            temporaryFirstName = firstName;
+            firstName = lastName;
+            lastName = temporaryFirstName;
+
+            Console.WriteLine($"firstname: {firstName}, lastname: {lastName}");
+            Console.ReadKey();
+        }
+    }
+}
