5f2616f7cb8e42af36d1dc041f4a64d2966b5ddc
snowgoos <xrustjasij@gmail.com>
2023-03-22 23:01:25 +0200

    Homework4

---
+++ Homework4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ijunior
+{
+    internal class Homework4
+    {
+        static void Main(string[] args)
+        {
+            int images = 52;
+            int imagesInRow = 3;
+            int filledRows;
+            int extraImages;
+
+            filledRows = images / imagesInRow;
+            extraImages = images % imagesInRow;
+
+            Console.WriteLine($"Total filled rows: {filledRows} and extra images: {extraImages}");
+            Console.ReadKey();
+        }
+    }
+}
