9ce5c5eba4016799e4de4356856e5f3eee21531f
snowgoos <xrustjasij@gmail.com>
2023-03-22 22:22:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ijunior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isActive = false;
+            char playerRank = 'A';
+            string playerName = "Player";
+            byte progressInPercentage = 19;
+            ushort crystalCount = 0;
+            short playerHealth = 100;
+            uint goldCount = 100000;
+            int fireResist = 5;
+            ulong playerExperience = 100000000;
+            float damage = 15f;
+        }
+    }
+}
