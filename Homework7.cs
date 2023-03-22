79e3d3db3f3911d72e01cceeba58b7a6c4a1e417
snowgoos <xrustjasij@gmail.com>
2023-03-22 23:34:28 +0200

    Homework7

---
+++ Homework7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ijunior
+{
+    internal class Homework7
+    {
+        static void Main(string[] args)
+        {
+            int peopleInQueue;
+            int serviceTimeInMinutes = 10;
+            int minutesInHour = 60;
+            int waitingTime;
+            int waitingHours;
+            int waitingMinutes;
+
+            Console.Write("Please enter people count in queue: ");
+            peopleInQueue = Convert.ToInt32(Console.ReadLine());
+
+            waitingTime = peopleInQueue * serviceTimeInMinutes;
+            waitingHours = waitingTime / minutesInHour;
+            waitingMinutes = waitingTime % minutesInHour;
+
+            Console.WriteLine($"Total waiting time in queue {waitingHours}h {waitingMinutes}m");
+        }
+    }
+}
