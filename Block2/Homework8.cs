a08515781cd38d754e83686d5ad8ff112977d682
snowgoos <xrustjasij@gmail.com>
2023-03-26 16:43:51 +0300

    Block2 - Homework8

---
+++ Block2/Homework8.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ijunior.Block2
+{
+    internal class Homework8
+    {
+        static void Main(string[] args)
+        {
+            string passwordForHome = "12345";
+            string userPassword;
+            byte passwordTriesLimit = 3;
+
+            for (int i = 0; i < passwordTriesLimit; i++)
+            {
+                Console.Write("Please enter password: ");
+                userPassword = Console.ReadLine();
+
+                if (userPassword == passwordForHome)
+                {
+                    Console.WriteLine("Password is correct. You are welcome!");
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password, please try again");
+                }
+            }
+
+        }
+    }
+}
