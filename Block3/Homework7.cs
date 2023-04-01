2ff539869fc9d7b07a7c3ad7faddab811e23660a
snowgoos <xrustjasij@gmail.com>
2023-04-02 00:58:47 +0300

    Block3 - Homework7

---
+++ Block3/Homework7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ijunior.Block3
+{
+    internal class Homework7
+    {
+        static void Main(string[] args)
+        {
+            string[] words;
+            string userInput;
+
+            userInput = Console.ReadLine();
+            words = userInput.Split(" ");
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
