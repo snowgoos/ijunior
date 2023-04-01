a381074fb18e39cdd73681580e9e6c6d41012173
snowgoos <xrustjasij@gmail.com>
2023-04-01 22:43:03 +0300

    Block3 - Homework3

--- Block3/Homework3.cs
+++ Block3/Homework3.cs
@@ -14,7 +14,11 @@ namespace ijunior.Block3
             Random random = new Random();
             int minRange = 10;
             int maxRange = 41;
-            
+            int firstIndex = 0;
+            int lastIndex = numbers.Length - 1;
+            int secondIndex = firstIndex + 1;
+            int penultIndex = lastIndex - 1;
+
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = random.Next(minRange, maxRange);
@@ -22,7 +26,29 @@ namespace ijunior.Block3
                 Console.Write($"{numbers[i]} ");
             }
 
-            Console.WriteLine();
+            Console.WriteLine("\nList with local max:");
+
+            if (numbers[firstIndex] > numbers[secondIndex])
+            {
+                Console.WriteLine(numbers[firstIndex]);
+            }
+
+            for (int i = 1; i < lastIndex; i++)
+            {
+                int previousIndex = i - 1;
+                int nextIndex = i + 1;
+
+                if (numbers[previousIndex] < numbers[i] && numbers[i] > numbers[nextIndex])
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+
+            if (numbers[lastIndex] > numbers[penultIndex])
+            {
+                Console.WriteLine(numbers[lastIndex]);
+            }
+
         }
     }
 }
