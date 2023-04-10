2c461f9d5004043f01f738e79d091521732d58cd
snowgoos <xrustjasij@gmail.com>
2023-04-10 10:56:29 +0300

    Block4 - Homework3

--- Block4/Homework3.cs
+++ Block4/Homework3.cs
@@ -10,7 +10,32 @@ namespace ijunior.Block4
     {
         static void Main(string[] args)
         {
-            
+            bool isParsed = false;
+
+            while (isParsed == false)
+            {
+                parsedStringToInt(ref isParsed);
+            }
+        }
+
+        static void parsedStringToInt(ref bool isParsed)
+        {
+            string userInput;
+            int parsedNumber;
+
+            Console.Write("Please enter number: ");
+            userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out parsedNumber))
+            {
+                isParsed = true;
+
+                Console.WriteLine(parsedNumber);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value entered");
+            }
         }
     }
 }
