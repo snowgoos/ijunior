e4c1621e5acfe40baf034c5bdc579904e43fc844
snowgoos <xrustjasij@gmail.com>
2023-04-11 13:21:04 +0300

    Block4 - Homework5

--- Block4/Homework5.cs
+++ Block4/Homework5.cs
@@ -10,12 +10,52 @@ namespace ijunior.Block4
     {
         static void Main(string[] args)
         {
-            
+            int[] numbers = new int[10];
+
+            RandomlyFillArray(numbers);
+            OutputArray(numbers);
+            ArrayShuffle(numbers);
+            OutputArray(numbers);
         }
 
-        static void ArrayShufle()
+        static void RandomlyFillArray(int[] numbers)
         {
-            
+            Random random = new Random();
+            int minRange = 1;
+            int maxRange = 10;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minRange, maxRange);
+            }
+        }
+
+        static void OutputArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void ArrayShuffle(int[] numbers)
+        {
+            int tempNumber;
+            Random random = new Random();
+            int currentIndex = numbers.Length;
+            int randomIndex;
+
+            while (currentIndex != 0)
+            {
+                randomIndex = random.Next(currentIndex);
+                currentIndex--;
+
+                tempNumber = numbers[randomIndex];
+                numbers[randomIndex] = numbers[currentIndex];
+                numbers[currentIndex] = tempNumber;
+            }
         }
     }
 }
