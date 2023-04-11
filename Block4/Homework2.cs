b49d76baa665b8b4240554b349689004a3f09485
snowgoos <xrustjasij@gmail.com>
2023-04-11 12:53:29 +0300

    Block4 - Homework2: incorrect task understanding, reworked code

--- Block4/Homework2.cs
+++ Block4/Homework2.cs
@@ -11,31 +11,39 @@ namespace ijunior.Block4
         static void Main(string[] args)
         {
             int maxValue = 10;
-            int currentValue = 4;
+            int currentPercentageValue = 40;
             int positionX = 5;
             int positionY = 10;
 
-            DrawBar(currentValue, maxValue, positionX, positionY);
+            DrawBar(currentPercentageValue, maxValue, positionX, positionY);
         }
 
-        static void DrawBar(int value, int maxValue, int positionX = 0, int positionY = 0)
+        static void DrawBar(int valueInPercentage, int maxValue, int positionX = 0, int positionY = 0)
         {
+            int currentValue = getWholePercentage(maxValue, valueInPercentage);
+
             Console.SetCursorPosition(positionY, positionX);
             Console.Write("[");
 
-            for (int i = 0; i < maxValue; i++)
-            {
-                if (i < value)
-                {
-                    Console.Write("#");
-                }
-                else
-                {
-                    Console.Write("_");
-                }
-            }
+            FillBar(currentValue, '#');
+            FillBar(maxValue, '_', currentValue);
 
             Console.Write("]");
+        }
+
+        static void FillBar(int maxValue, char symbol, int startValue = 0)
+        {
+            for (int i = startValue; i < maxValue; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+
+        static int getWholePercentage(int value, int percentage)
+        {
+            float percentageValue = Convert.ToSingle(percentage) / 100 * value;
+
+            return Convert.ToInt32(percentageValue);
         }
     }
 }
