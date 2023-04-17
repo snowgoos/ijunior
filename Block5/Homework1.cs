0d024f8d944da57920e2f327ed265a300556a917
snowgoos <xrustjasij@gmail.com>
2023-04-18 00:24:59 +0300

    Block5 - Homework1: removed unused code

--- Block5/Homework1.cs
+++ Block5/Homework1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ijunior.Block5
+﻿namespace ijunior.Block5
 {
     internal class Homework1
     {
