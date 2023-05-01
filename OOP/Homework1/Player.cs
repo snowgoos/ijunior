724911e8a2e29e75d7dfaca3e4c698144b10972f
snowgoos <xrustjasij@gmail.com>
2023-05-01 12:17:35 +0300

    OOP - Player

---
+++ OOP/Homework1/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace ijunior.OOP.Homework1
+{
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int Damage { get; private set; }
+
+        public Player(string name, int health, int damage)
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Health: {Health}");
+            Console.WriteLine($"Damage: {Damage}");
+        }
+    }
+}
---
+++ OOP/Homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ijunior.OOP.Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Player", 100, 10);
+
+            player.ShowInfo();
+        }
+    }
+}
